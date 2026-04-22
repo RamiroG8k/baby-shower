0035143a2fff5b15a68dc3618c7ac9ae6f2f7c57
Ramiro Mendez <Ramiro.Mendez@contracted.smpmi.com>
2026-04-22 13:28:54 -0600

    Move to use svgs directly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import icon from 'astro-icon';
 
 export default defineConfig({
   output: 'server',
   adapter: node({ mode: 'standalone' }),
+  integrations: [icon()],
 });

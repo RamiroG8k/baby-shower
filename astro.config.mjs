4baf1d88a29e4d929eff39a2ffe85983cd7b07d4
Ramiro Mendez <Ramiro.Mendez@contracted.smpmi.com>
2026-05-01 14:39:11 -0600

    Remove node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import icon from "astro-icon";
 
 export default defineConfig({
-  output: "server",
-  adapter: node({ mode: "standalone" }),
+  output: "static",
   integrations: [icon()],
 });
